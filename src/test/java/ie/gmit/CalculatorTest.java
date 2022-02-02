f962961786130da1c2555618a359f08d9e26383e
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-02-02 09:27:16 +0000

    Calculator add

---
+++ src/test/java/ie/gmit/CalculatorTest.java
@@ -0,0 +1,15 @@
+package ie.gmit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalculatorTest {
+    Calculator c = new Calculator();
+
+    @Test
+    void addTest() {
+        assertTrue(c.add(5, 5) == 10);
+    }
+
+}
